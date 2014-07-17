81ad94c396c09575fa17ff2398023d24bd606319
Sourabh Dev <sourabh.dev90@gmail.com>
2014-07-17 22:40:17 +0530

    Derived the sentiment of each tweet

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -6,12 +7,37 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+def tweet_score(fp, tp):
+    afinnfile = open(fp)
+    tweet_file = open(tp)
+    scores = {}
+    tweets = []
+
+    for line in afinnfile:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    for tweet in tweet_file:
+        parse_tweet = json.loads(tweet, 'utf-8')
+        if ('text' in parse_tweet):
+            tweets.append(parse_tweet[u'text'])
+
+    for tweet in tweets:
+        sentiment_value = 0
+        for word in scores.keys():
+            sentiment_value += (tweet.encode('utf-8').count(word)*scores[word])
+        print(sentiment_value)
+
+    # print tweets
+    # print scores.items()
+
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
+    tweet_score(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
     main()
